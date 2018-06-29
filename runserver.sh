00f74b084d8439f3f361db803c207916ff035271
Ajoy Oommen <ajoyoommen@gmail.com>
2018-06-29 13:05:37 +0530

    Add driver for ODBC 17

--- runserver.sh
+++ runserver.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /appl/flask_demo/project
-export DATABASE_URL='mssql+pyodbc://SA:2tr0ngpassw0rd!@172.20.16.19:1433/flask_demo'
+export DATABASE_URL='mssql+pyodbc://SA:2tr0ngpassw0rd!@172.20.11.76:1433/flask_demo?driver=ODBC+Driver+17+for+SQL+Server'
 nohup gunicorn -b 0.0.0.0:8000 run:app > ../logs/gunicorn.log
