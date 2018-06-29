cee76c7b0a38330646f571d86e7420fc5b6af58f
Ajoy Oommen <ajoyoommen@gmail.com>
2018-06-29 12:19:48 +0530

    Add Dockerfile for server with ODBC installed

--- project/instance/config.py
+++ project/instance/config.py
@@ -1,5 +1,8 @@
 import os
+
+
 PROJECT_DIR = os.path.abspath(__file__).rsplit('/',3)[0]
+
 
 class Config(object):
     """Parent configuration class."""
@@ -9,26 +12,29 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+
 class DevelopmentConfig(Config):
     """Configurations for Development."""
     DEBUG = True
 
+
 class TestingConfig(Config):
     """Configurations for Testing, with a separate test database."""
     TESTING = True
-    #SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/test_db'
     SQLALCHEMY_DATABASE_URI = 'sqlite:////' + PROJECT_DIR + '/test_db/polls.db'
-    print('SQLALCHEMY_DATABASE_URI', SQLALCHEMY_DATABASE_URI)
     DEBUG = True
+
 
 class StagingConfig(Config):
     """Configurations for Staging."""
     DEBUG = True
 
+
 class ProductionConfig(Config):
     """Configurations for Production."""
     DEBUG = False
     TESTING = False
+
 
 app_config = {
     'development': DevelopmentConfig,
--- runserver.sh
+++ runserver.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /appl/flask_demo/project
-
+export DATABASE_URL='mssql+pyodbc://SA:2tr0ngpassw0rd!@172.20.16.19:1433/flask_demo'
 nohup gunicorn -b 0.0.0.0:8000 run:app > ../logs/gunicorn.log
