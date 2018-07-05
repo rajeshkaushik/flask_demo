babf2d52bc30b099900cc5a31dbc29bddf70652f
Ajoy Oommen <ajoyoommen@gmail.com>
2018-07-05 18:21:13 +0530

    Feature sql server integration (#3)
    * Add Dockerfile for server with ODBC installed
    
    * Add driver for ODBC 17
    
    * Update docker file for application
    
    * Add dockerfile for MS SQL
    
    * Update README
    
    * gunicorn conf added
    
    * Use python image in docker file
    
    * gunicorn logging enabled
    
    * docker network added
    
    * Update README and docker files
    
    * Update env_vars.sh

--- Dockerfile
+++ Dockerfile
@@ -1,28 +1,25 @@
-FROM python:3.6.5-jessie
+FROM python:3.6.6-slim-stretch
 
-RUN sed -i "s/jessie main/jessie main contrib non-free/" /etc/apt/sources.list
+RUN apt-get update && \
+    apt-get install -y curl apt-transport-https &&\
+    curl http://packages.microsoft.com/config/debian/9/prod.list > \
+    /etc/apt/sources.list.d/mssql-release.list
 
-RUN echo "deb http://http.debian.net/debian jessie-backports main contrib non-free" >> /etc/apt/sources.list
+RUN apt-get update \
+    && apt-get install -y \
+        build-essential && \
+        apt-get install -y curl unixodbc-dev \
+        && ACCEPT_EULA=Y apt-get install -y --allow-unauthenticated msodbcsql17
 
-RUN apt-get update
 
-RUN apt-get install -y python3-dev
+RUN mkdir -p /appl/flask_demo/logs
+RUN mkdir -p /var/log/gunicorn
 
-RUN mkdir /appl
-RUN mkdir /appl/flask_demo
-RUN mkdir /appl/flask_demo/logs
-RUN mkdir /appl/flask_demo/projects
+COPY . /appl/flask_demo/
 
-RUN touch /appl/flask_demo/logs/gunicorn.log
-
-COPY project/ /appl/flask_demo/project
-COPY runserver.sh /appl/flask_demo/
-
-WORKDIR /appl/flask_demo
-
-RUN pip install -r project/requirements.txt
-RUN pip install gunicorn
+WORKDIR /appl/flask_demo/project
+RUN pip3 install -r requirements.txt
 
 EXPOSE 8000
 
-CMD /appl/flask_demo/runserver.sh
+ENTRYPOINT ["gunicorn", "-c", "gunicorn_config.py", "wsgi:app"]
--- project/instance/config.py
+++ project/instance/config.py
@@ -1,34 +1,58 @@
 import os
+
+
 PROJECT_DIR = os.path.abspath(__file__).rsplit('/',3)[0]
+
+
+HOST = os.getenv('APP_HOST', '0.0.0.0')
+PORT = os.getenv('APP_PORT', '8000')
+
+
 
 class Config(object):
     """Parent configuration class."""
     DEBUG = False
     CSRF_ENABLED = True
     SECRET = os.getenv('SECRET', 'This is my secret')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+
+    DB = {
+        'engine': 'mssql+pyodbc',
+        'user': os.getenv('DB_USER'),
+        'pass': os.getenv('DB_PASS'),
+        'host': os.getenv('DB_HOST'),
+        'port': os.getenv('DB_PORT'),
+        'name': os.getenv('DB_NAME'),
+        'driver': 'ODBC+Driver+17+for+SQL+Server'
+    }
+
+    SQLALCHEMY_DATABASE_URI = (
+        '%(engine)s://%(user)s:%(pass)s@%(host)s:%(port)s/%(name)s?driver=%(driver)s'
+    ) % DB
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
 class DevelopmentConfig(Config):
     """Configurations for Development."""
     DEBUG = True
 
+
 class TestingConfig(Config):
     """Configurations for Testing, with a separate test database."""
     TESTING = True
-    #SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/test_db'
     SQLALCHEMY_DATABASE_URI = 'sqlite:////' + PROJECT_DIR + '/test_db/polls.db'
-    print('SQLALCHEMY_DATABASE_URI', SQLALCHEMY_DATABASE_URI)
     DEBUG = True
+
 
 class StagingConfig(Config):
     """Configurations for Staging."""
     DEBUG = True
 
+
 class ProductionConfig(Config):
     """Configurations for Production."""
     DEBUG = False
     TESTING = False
+
 
 app_config = {
     'development': DevelopmentConfig,
