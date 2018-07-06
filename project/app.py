6d78f756ed3c306d49832d9e07fedc6c7d3d7006
Ajoy Oommen <ajoyoommen@gmail.com>
2018-07-06 14:17:53 +0530

    Update project/app.py

--- project/app.py
+++ project/app.py
@@ -1,7 +1,6 @@
 import os
 
 from flask import Flask
-# from flask_restful import Api
 from flask_restplus import Api
 from flask_sqlalchemy import SQLAlchemy
 
@@ -9,14 +8,11 @@ from flask_sqlalchemy import SQLAlchemy
 from instance.config import app_config
 
 
-# initialize sql-alchemy
-db = SQLAlchemy()
-
-# def create_app(config_name):
-config_name = os.getenv('APP_SETTINGS', 'development') # config_name = "development"
-
 app = Flask(__name__)
+config_name = os.getenv('APP_SETTINGS', 'development')
 app.config.from_object(app_config[config_name])
+
+db = SQLAlchemy()
 db.init_app(app)
 api = Api(app)
 
@@ -27,4 +23,3 @@ api.add_resource(QuestionListApi, '/questions')
 api.add_resource(QuestionApi, '/questions/<int:id>')
 
 api.add_resource(UsersApi, '/users')
-    # return app
